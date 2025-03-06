a7f7e00927791cf300d09941517935cd75956e68
Parvez <74895218+parvez-tm@users.noreply.github.com>
2025-03-06 23:20:41 +0530

    test 3

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors({
   }));
 
 app.get('/hello',(req,res)=>{
-    res.send('Hello Ptm K http://141.148.201.28/');
+    res.send('Hello Ptm from http://141.148.201.28/');
 })
 app.use(express.json())
 // app.use(require('./routes/studentRoutes'))
